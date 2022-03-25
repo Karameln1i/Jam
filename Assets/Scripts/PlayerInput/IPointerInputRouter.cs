8846ee9c5982f9a5b89690b9b53b8fc765d1e661
Karameln1i <76053369+Karameln1i@users.noreply.github.com>
2022-03-26 01:01:11 +0300

    Накидал сцену и добавил скрипты для управления

---
+++ Assets/Scripts/PlayerInput/IPointerInputRouter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public interface IPointerInputRouter
+{
+    Vector2 Position { get; }
+    event Action Pressed;
+    event Action Moved;
+    event Action Released;
+}
